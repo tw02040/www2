60051c460fb891367ccc0ab0e8a246d48df7943b
tw02040 <tw02040@naver.com>
2020-11-09 15:30:49 +0900

    Update dbconn.php

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,5 +1,5 @@
 <?
-    $connect=mysql_connect( "localhost", "ahneunsu", "eunsu0307^^") or  
+    $connect=mysql_connect( "localhost", "ahneunsu", "password") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("ahneunsu",$connect);
